1893e40afcb937d325335bc0ba80c24c47ee9bca
Doston Ismailov <dostonberuniy@gmail.com>
2025-07-07 07:56:42 +0500

    readme and user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,11 +13,27 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'password' => bcrypt('test'),
+            'name' => 'Ivan Ivanov',
+            'email' => 'ivan@example.com',
+            'password' => bcrypt('pmp2025'),
         ]);
 
-        User::factory(20)->create();
+        User::factory()->create([
+            'name' => 'Aleksey Ivanov',
+            'email' => 'aleksey@example.com',
+            'password' => bcrypt('pmp2025'),
+        ]);
+
+        User::factory()->create([
+            'name' => 'Andrey Ivanov',
+            'email' => 'andrey@example.com',
+            'password' => bcrypt('pmp2025'),
+        ]);
+
+        User::factory()->create([
+            'name' => 'Sergey Ivanov',
+            'email' => 'sergey@example.com',
+            'password' => bcrypt('pmp2025'),
+        ]);
     }
 }
