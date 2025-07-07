47307ded1e665dca49f1dee5b52f66d6874494a9
Doston Ismailov <dostonberuniy@gmail.com>
2025-07-07 07:59:39 +0500

    dashboard redirect

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [\App\Http\Controllers\DashboardController::class, 'index'])->name('home')->middleware('auth');
+//Route::get('/', [\App\Http\Controllers\DashboardController::class, 'index'])->name('home')->middleware('auth');
+Route::redirect('/', 'project')->name('home')->middleware('auth');
